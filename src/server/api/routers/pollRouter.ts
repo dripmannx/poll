ff572a823db465dc3ece9819aae1f822f6e061d9
Luca <luca.duepmann@gmail.com>
2023-07-12 19:28:42 +0200

    Added procedure to create Poll with choices

--- src/server/api/routers/pollRouter.ts
+++ src/server/api/routers/pollRouter.ts
@@ -1,11 +1,12 @@
 import dayjs from "dayjs";
-import { nanoid } from "nanoid";
-import { z } from "zod";
+import { customAlphabet, nanoid } from "nanoid";
+import { number, z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 export const pollRouter = createTRPCRouter({
   getAllPollsByCreatedAt: publicProcedure.query(({ ctx }) => {
     return ctx.prisma.poll.findMany({
       orderBy: { createdAt: "asc" },
+      include: { choices: true },
     });
   }),
   getPollById: publicProcedure
@@ -21,19 +22,31 @@ export const pollRouter = createTRPCRouter({
         question: z.string(),
         public: z.boolean(),
         expire: z.date().optional(),
+        choices: z.object({ choicesText: z.string() }).array(),
       })
     )
     .mutation(async ({ ctx, input }) => {
       const now = dayjs();
       const b = now.add(10, "years");
-      const link = nanoid(9);
+
+      const link2 = customAlphabet(
+        "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+        8
+      );
       await ctx.prisma.poll.create({
         data: {
-          link: link,
+          link: link2(),
           question: input.question,
           public: input.public,
           expiredAt: input.expire ? input.expire : b.toDate(),
           willExpire: input.expire ? false : true,
+          choices: {
+            createMany: {
+              data: input.choices.map((choice) => ({
+                choiceText: choice.choicesText,
+              })),
+            },
+          },
         },
       });
     }),
